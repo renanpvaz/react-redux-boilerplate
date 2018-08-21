8275910990e996a4fb59f9b90bdba2bd82bfb3c7
renanpvaz <renan.piresvz@gmail.com>
2018-08-21 16:18:06 -0300

    Configure basic routing with redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<div />, document.getElementById('root'))
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { createBrowserHistory } from 'history'
+import { applyMiddleware, compose, createStore } from 'redux'
+import { connectRouter, routerMiddleware } from 'connected-react-router'
+
+import { Provider } from 'react-redux'
+import { Route, Switch } from 'react-router' // react-router v4
+import { ConnectedRouter } from 'connected-react-router'
+
+import rootReducer from './store'
+
+const history = createBrowserHistory()
+
+const store = createStore(
+  connectRouter(history)(rootReducer),
+  compose(
+    applyMiddleware(
+      routerMiddleware(history),
+      // more middlewares here
+    ),
+  ),
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <Switch>
+          <Route exact path="/" render={() => (<div>Match</div>)} />
+          <Route render={() => (<div>Miss</div>)} />
+        </Switch>
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
 
 registerServiceWorker()
